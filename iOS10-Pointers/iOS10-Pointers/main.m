b3329aa6b9cb279b635f76caf73be7598efc6d6d
Paul Solt <PaulSolt@gmail.com>
2019-12-12 12:24:18 -0500

    Added pointers project

---
+++ iOS10-Pointers/iOS10-Pointers/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  iOS10-Pointers
+//
+//  Created by Paul Solt on 12/12/19.
+//  Copyright © 2019 Lambda, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+	return 0;
+}

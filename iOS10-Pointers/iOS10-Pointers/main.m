3fd5191128c0f11d06acb7dd35709c7fc5a5affa
Paul Solt <PaulSolt@gmail.com>
2019-12-12 12:39:45 -0500

    Starting to explore long memory

--- iOS10-Pointers/iOS10-Pointers/main.m
+++ iOS10-Pointers/iOS10-Pointers/main.m
@@ -10,8 +10,24 @@
 
 int main(int argc, const char * argv[]) {
 	@autoreleasepool {
-	    // insert code here...
-	    NSLog(@"Hello, World!");
+
+		printf("Hello world!\n");
+		
+		char letter = 'x';
+		printf("Letter: %c\n", letter);
+		
+		// Primitive Types
+		// float, int, double, char, long
+		
+		float float_pi = 3.14159265359;
+		double pi = 3.14159265359;
+		
+		printf("pi: %f\n", pi);
+		printf("pi: %0.3f\n", pi);
+		
+		long value = 42;
+		printf("value: %li\n", value);
+		
 	}
 	return 0;
 }
